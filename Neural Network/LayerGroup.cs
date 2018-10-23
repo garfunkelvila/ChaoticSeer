70a8e13bc3d35e3f1b4ae53d39381fbdf704214b
Garfunkel <garfunkelvila@outlook.com>
2018-10-23 12:13:37 +0800

    Changed properties into fields
    Changed properties into fields

--- Neural Network/LayerGroup.cs
+++ Neural Network/LayerGroup.cs
@@ -10,12 +10,20 @@ namespace Neural_Network {
     class LayerGroup {
         public delegate void OutputHandler (double[] motors);
         public event OutputHandler Decision;
+        bool isSingle;
+
+        public NeuronLayer[] NeuronLayers;
+        public NeuronLayer NeuronLayer;
+        public double[] Inputs;
+        public double[] Outputs;
+
         #region Constructors
         /// <summary>
         /// Holds an array of Neuron layers.
         /// </summary>
         /// <param name="nLayers"></param>
         public LayerGroup (NeuronLayer[] nLayers) {
+            isSingle = false;
             NeuronLayers = nLayers;
         }
         /// <summary>
@@ -23,12 +31,42 @@ namespace Neural_Network {
         /// </summary>
         /// <param name="nLayers"></param>
         public LayerGroup (NeuronLayer nLayer) {
+            isSingle = true;
             NeuronLayer = nLayer;
         }
         #endregion
-        public NeuronLayer[] NeuronLayers { get; set; }
-        public NeuronLayer NeuronLayer { get; set; }
-        public double[] Inputs { get; set; }
-        public double[] Outputs { get; set; }
+        public void Decide () {
+            double[] outputBuffer = new double[Inputs.Count()];
+
+            if (isSingle) {
+                //Give every inputs to every neurons, each dendrites
+                for (int n = 0; n < Inputs.Length; n++) {
+                    for (int d = 0; d < NeuronLayer.neurons[n].Dendrites.Length; d++) {
+                        for (int i = 0; i < Inputs.Length; i++) {
+                            NeuronLayer.neurons[n].Dendrites[d] = Inputs[i];
+                        }
+                    }
+                }
+                //Get outputs
+                for (int i = 0; i < Inputs.Length; i++) {
+                    outputBuffer[i] = NeuronLayer.neurons[i].Axon();
+                }
+                Decision.Invoke(outputBuffer);
+            }
+            else {
+                //Process each layer
+                //Set the output of process to next layer
+                foreach (NeuronLayer nL in NeuronLayers) {
+                    //Put the layer result in the buffer
+                    for (int i = 0; i < Inputs.Count(); i++) {
+                        outputBuffer[i] = nL.neurons[i].Axon();
+                    }
+
+                    //Set the buffer as the input
+                    //Replace each item as per output?
+                }
+                Decision.Invoke(outputBuffer);
+            }
+        }
     }
 }
