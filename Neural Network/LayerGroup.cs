ea76dba5ab23eed525a502ecbe86fe9dcfb2a7ca
Garfunkel <garfunkelvila@outlook.com>
2018-10-24 16:59:30 +0800

    First attemp for the layer group

--- Neural Network/LayerGroup.cs
+++ Neural Network/LayerGroup.cs
@@ -5,12 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Neural_Network {
-    //LayerGroup should be able to connect with a layer group with same output:input
-    //
     class LayerGroup {
         public delegate void OutputHandler (double[] motors);
         public event OutputHandler Decision;
-        bool isSingle;
+        bool isSingleNl; //Might remove this and just use array
 
         public NeuronLayer[] NeuronLayers;
         public NeuronLayer NeuronLayer;
@@ -23,7 +21,7 @@ namespace Neural_Network {
         /// </summary>
         /// <param name="nLayers"></param>
         public LayerGroup (NeuronLayer[] nLayers) {
-            isSingle = false;
+            isSingleNl = false;
             NeuronLayers = nLayers;
         }
         /// <summary>
@@ -31,39 +29,61 @@ namespace Neural_Network {
         /// </summary>
         /// <param name="nLayers"></param>
         public LayerGroup (NeuronLayer nLayer) {
-            isSingle = true;
+            isSingleNl = true;
             NeuronLayer = nLayer;
         }
         #endregion
         public void Decide () {
-            double[] outputBuffer = new double[Inputs.Count()];
+            double[] outputBuffer = new double[Inputs.Count()]; //I think this sould equal to axons or neurons of last layer
 
-            if (isSingle) {
-                //Give every inputs to every neurons, each dendrites
-                for (int n = 0; n < Inputs.Length; n++) {
+            if (isSingleNl) {
+                //Feed Input
+                for (int n = 0; n < NeuronLayer.neurons.Length; n++) {
                     for (int d = 0; d < NeuronLayer.neurons[n].Dendrites.Length; d++) {
                         for (int i = 0; i < Inputs.Length; i++) {
                             NeuronLayer.neurons[n].Dendrites[d] = Inputs[i];
                         }
                     }
                 }
+
                 //Get outputs
-                for (int i = 0; i < Inputs.Length; i++) {
-                    outputBuffer[i] = NeuronLayer.neurons[i].Axon();
+                for (int o = 0; o < Inputs.Length; o++) {
+                    outputBuffer[o] = NeuronLayer.neurons[o].Axon();
                 }
+
                 Decision.Invoke(outputBuffer);
             }
             else {
-                //Process each layer
-                //Set the output of process to next layer
-                foreach (NeuronLayer nL in NeuronLayers) {
-                    //Put the layer result in the buffer
-                    for (int i = 0; i < Inputs.Count(); i++) {
-                        outputBuffer[i] = nL.neurons[i].Axon();
-                    }
+                //Layer > Neuron > Dendrites > Inputs
 
-                    //Set the buffer as the input
-                    //Replace each item as per output?
+                //Feed Input on the first layer and put into buffer
+                for (int n = 0; n < NeuronLayers[0].neurons.Length; n++) {
+                    for (int d = 0; d < NeuronLayers[0].neurons[n].Dendrites.Length; d++) {
+                        for (int i = 0; i < Inputs.Length; i++) {
+                            NeuronLayers[0].neurons[n].Dendrites[d] = Inputs[i];
+                        }
+                    }
+                }
+                for (int i = 0; i < Inputs.Length; i++) {
+                    outputBuffer[i] = NeuronLayer.neurons[i].Axon();
+                }
+
+                //Get outputs then feed into next layer
+                for (int nL = 1; nL < NeuronLayers.Length; nL++) {
+                    for (int n = 0; n < NeuronLayers[nL].neurons.Length; n++) {
+                        for (int d = 0; d < NeuronLayers[nL].neurons[n].Dendrites.Length; d++) {
+                            for (int i = 0; i < Inputs.Length; i++) {
+                                NeuronLayers[nL].neurons[n].Dendrites[d] = Inputs[i];
+                            }
+                        }
+                    }
+                    //I think this is still WRONG, Im lost xD
+                    //i am not sure how do i feed the output of last layer to next
+                    
+                    //I think output buffer should be re initialized based on dendrite count
+                    for (int i = 0; i < Inputs.Length; i++) {
+                        outputBuffer[i] = NeuronLayer.neurons[i].Axon();
+                    }
                 }
                 Decision.Invoke(outputBuffer);
             }
