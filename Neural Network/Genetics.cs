e82ac19de813fea65f2401b1b4e1614d18130abd
Garfunkel <garfunkelvila@outlook.com>
2018-10-30 16:20:35 +0800

    Planed to contain genetic functions, or at least mutation

---
+++ Neural Network/Genetics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network {
+    class Genetics {
+        public void Mutate () {
+
+        }
+    }
+}
