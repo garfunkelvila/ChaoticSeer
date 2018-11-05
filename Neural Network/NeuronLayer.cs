ac18c72114939dc19bb8aee800187b0d0c5b1355
Garfunkel <garfunkelvila@outlook.com>
2018-11-05 22:30:39 +0800

    Changed fields into readonly

--- Neural Network/NeuronLayer.cs
+++ Neural Network/NeuronLayer.cs
@@ -1,23 +1,26 @@
-﻿using System;
+﻿//  Copyright (C) 2018  Garfunkel Vila
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// ALLOW SOMEHING LIKE THIS, ISOLATED LAYERS
-// Just realized that, what I want to do is a CNS
-// *-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*-*-*-*
-//         *-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*-*-*-*
-//         *-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*-*
-// *-*-*-*-*-*-*-*-*-*
 namespace Neural_Network {
+    //ISSUE when constructed in a Threading.Parallel.For it keeps giving -1 for weight or +1, not sure if its this fault or Nueron,
+    //or it is because of activation not using static methods
     public enum NeuronTypes {
         Sensory = 1,
         Inter = 2,
@@ -27,34 +30,34 @@ namespace Neural_Network {
     /// Neuron layer or Layer group (non-existing yet) should accept one or many double and poop one or many double
     /// </summary>
     public class NeuronLayer {
-        public Neuron[] neurons;
-        public NeuronTypes neuronType;
+        readonly public Neuron[] neurons;
+        readonly public NeuronTypes neuronType;
+        readonly public int InputCount; //Used by BackPropagation
         /// <summary>
         /// Creates a layer filled with neurons
         /// </summary>
         /// <param name="nCount">Neuron count</param>
         /// <param name="iCount">Input count</param>
         public NeuronLayer (int iCount, int nCount) {
-            this.neurons = new Neuron[nCount];
+            neurons = new Neuron[nCount];
+            InputCount = iCount;
             for (int i = 0; i < nCount; i++) {
                 neurons[i] = new Neuron(iCount);
             }
         }
-        public NeuronLayer (int nCount, int iCount, NeuronTypes nType = NeuronTypes.Inter) {
-            this.neurons = new Neuron[nCount];
+        public NeuronLayer (int iCount, int nCount,  NeuronTypes nType = NeuronTypes.Inter) {
+            neurons = new Neuron[nCount];
+            InputCount = iCount;
             for (int i = 0; i < nCount; i++) {
                 neurons[i] = new Neuron(iCount);
             }
         }
-        public NeuronLayer (int nCount, int iCount, ActivationFunctions aF = ActivationFunctions.Logistic) {
-            this.neurons = new Neuron[nCount];
+        public NeuronLayer (int iCount, int nCount,  ActivationFunctions aF = ActivationFunctions.Logistic) {
+            neurons = new Neuron[nCount];
+            InputCount = iCount;
             for (int i = 0; i < nCount; i++) {
                 neurons[i] = new Neuron(iCount, aF);
             }
         }
-        //public Neuron[] neurons { get; set; }
-        //public NeuronTypes neuronType { get; set; }
-        //Add input
-        //Add output
     }
 }
