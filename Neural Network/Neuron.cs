5ab57cae64876fc8f5070c87c25915ac74e5e64f
Garfunkel <garfunkelvila@outlook.com>
2018-11-05 22:29:55 +0800

    Added license and copyright informations

--- Neural Network/Neuron.cs
+++ Neural Network/Neuron.cs
@@ -1,22 +1,37 @@
-﻿using System;
+﻿//  Copyright (C) 2018  Garfunkel Vila
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Neural_Network {
-    public class Neuron : Activations {
+    public class Neuron : Activations{
         public double[] Dendrites;
         public double[] Weights;
         public double Bias;
-        public ActivationFunctions ActivationFunction;
+        public double cAxon; //Used by BackPropagation
+        readonly public ActivationFunctions ActivationFunction;
         public Neuron (
                 int dendritesCount,
                 ActivationFunctions af = ActivationFunctions.TanH) {
             Dendrites = new double[dendritesCount];
             Weights = new double[dendritesCount];
             for (int i = 0; i < dendritesCount; i++)
-                Weights[i] = r.NextDouble() * 2 - 1;
+                Weights[i] = r.NextDouble() * 1 - 1;
             Bias = r.NextDouble() * 2 - 1;
             ActivationFunction = af;
         }
