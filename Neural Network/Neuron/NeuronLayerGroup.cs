22bd7ceabf762e8b8978cd5f1d50dacff98e37b1
Garfunkel <garfunkelvila@outlook.com>
2018-11-19 22:18:47 +0800

    Changed how output is buffered. and how neurons are getting their inputs

--- Neural Network/Neuron/NeuronLayerGroup.cs
+++ Neural Network/Neuron/NeuronLayerGroup.cs
@@ -33,34 +33,48 @@ namespace Neural_Network {
         #endregion
         public float[] Predict (float[] Inputs) {
             float[] outputBuffer;
+
+            // Feed the input to the input layer
             for (int n = 0; n < NeuronLayers[0].neurons.Length; n++) {
                 for (int d = 0; d < NeuronLayers[0].neurons[n].Dendrites.Length; d++) {
                     NeuronLayers[0].neurons[n].Dendrites[d] = Inputs[d];
                 }
             }
+            
+            // Fill the prediction for the input layer
             outputBuffer = new float[NeuronLayers[0].neurons.Length];
             for (int oB = 0; oB < outputBuffer.Length; oB++) {
                 outputBuffer[oB] = NeuronLayers[0].neurons[oB].Axon();
             }
 
-            if (NeuronLayers.Length == 1) { //Return if there are no hidden lat
+            // Return if there are no other layer
+            if (NeuronLayers.Length == 1) { 
                 Prediction = outputBuffer;
                 return outputBuffer;
             }
 
-            //Get outputs then feed into next layer
+            
             for (int nL = 1; nL < NeuronLayers.Length; nL++) {
-                Parallel.For(0, NeuronLayers[nL].neurons.Length, n => {
+                // Feed the current layer the prediction of last layer
+                for (int n = 0; n < NeuronLayers[nL].neurons.Length; n++) {
                     for (int d = 0; d < NeuronLayers[nL].neurons[n].Dendrites.Length; d++) {
-                        NeuronLayers[nL].neurons[n].Dendrites[d] = outputBuffer[d];
+                        NeuronLayers[nL].neurons[n].Dendrites[d] = NeuronLayers[nL - 1].neurons[d].Prediction;
                     }
-                });
-                outputBuffer = new float[NeuronLayers[nL].neurons.Length];
+
+                }
+
+                // Fill prediction of current layer
                 //--------------------------------------------------------------------------
-                for (int oB = 0; oB < outputBuffer.Length; oB++) {
-                    outputBuffer[oB] = NeuronLayers[nL].neurons[oB].Axon();
+                for (int oB = 0; oB < NeuronLayers[nL].neurons.Length; oB++) {
+                    NeuronLayers[nL].neurons[oB].Axon();
                 }
             }
+            outputBuffer = new float[NeuronLayers[NeuronLayers.Length - 1].neurons.Length];
+            for (int n = 0; n < NeuronLayers[NeuronLayers.Length - 1].neurons.Length; n++) {
+                outputBuffer[n] = NeuronLayers[NeuronLayers.Length - 1].neurons[n].Prediction;
+            }
+            
+
             Prediction = outputBuffer;
             //Add event soon
             return outputBuffer;
