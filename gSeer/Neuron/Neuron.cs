140c1c9a71dc18fed27ba481cef92329438b5db6
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-04 17:56:01 +0800

    Remove activation dependency Add static random (will relocate random on other part) Usage of abstract

--- gSeer/Neuron/Neuron.cs
+++ gSeer/Neuron/Neuron.cs
@@ -23,7 +23,8 @@ using System.Threading.Tasks;
 using gSeer.Neuron.ActivationFunction;
 
 namespace gSeer.Neuron {
-    public class Neuron : Activation {
+    public class Neuron {
+        readonly private static Random r = new Random();
         readonly public Activation _activationFunction; //I assume that when this thing is readonly, affected scripts will skip jump instructions except the one with random
         readonly public float LearningRate;    // This is for mutation too
 
@@ -39,16 +40,15 @@ namespace gSeer.Neuron {
         /// <param name="dendritesCount">Input count</param>
         /// <param name="aF">Activation function to use</param>
         public Neuron (int dendritesCount, Activation aF = null) {
-            //I cannot set it to defualt so I use null for now
-            // Err: Default parameter for value must be a compile time constant
-            if (aF == null) aF = new Logistic();
-
             Dendrites = new float[dendritesCount];
             Weights = new float[dendritesCount];
             for (int i = 0; i < dendritesCount; i++)
                 Weights[i] = (float) r.NextDouble() * -0.1f; //Will change range dependent into AF
             Bias = 1; //(float) r.NextDouble();
-            _activationFunction = aF;
+
+            //I cannot set it to defualt so I use null for now
+            // Err: Default parameter for value must be a compile time constant
+            _activationFunction = aF == null ? new Logistic() : aF;
             LearningRate = (float) r.NextDouble() * 0.1f;
         }
         /// <summary>
@@ -61,7 +61,8 @@ namespace gSeer.Neuron {
                 netPrediction += Dendrites[i] * Weights[i];
             }
             netPrediction += Bias;
-            Prediction = CalcAxon(netPrediction);
+
+            Prediction = _activationFunction.GetAxon(netPrediction);// CalcAxon(netPrediction);
             return Prediction;
         }
         /// <summary>
@@ -69,7 +70,7 @@ namespace gSeer.Neuron {
         /// </summary>
         /// <returns></returns>
         public float AxonPrime() {
-            return CalcDerv(netPrediction);
+            return _activationFunction.GetDerv(netPrediction);
         }
         
     }
