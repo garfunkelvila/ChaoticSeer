58b127d0c58efbde86343831d6d8e45fbeed5b75
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-04 17:54:52 +0800

    Change function for the abstract

--- gSeer/Neuron/Activation.cs
+++ gSeer/Neuron/Activation.cs
@@ -10,5 +10,18 @@ namespace gSeer.Neuron {
         protected abstract float CalcAxon(float x, float y);
         protected abstract float CalcDerv(float x);
         protected abstract float CalcDerv(float x, float y);
+
+        public float GetAxon(float x) {
+            return CalcAxon(x);
+        }
+        public float GetAxon(float x, float y) {
+            return GetAxon(x, y);
+        }
+        public float GetDerv(float x) {
+            return GetDerv(x);
+        }
+        public float GetDerv(float x, float y) {
+            return GetDerv(x, y);
+        }
     }
 }
