9a15318c6ae2f661c100b8e5be2c89d75ff6bea2
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-06 18:26:26 +0800

    Revert "CHange to private, In future I will isolate these things"
    This reverts commit f2d66701fdf88c187a730eed660b2bfc5594ee11.

--- gSeer/Seer.cs
+++ gSeer/Seer.cs
@@ -23,7 +23,7 @@ using gSeer.Neuron;
 
 namespace gSeer {
     public class Seer {
-        private NeuronLayerGroup nlg;
+        public NeuronLayerGroup nlg;
 
         /// <summary>
         /// This seer creates a fully connected network. If you want to create a custom one, you may want to use Neuron, NeuronLayer and NeuronLayergroup to create what you want
@@ -55,12 +55,9 @@ namespace gSeer {
             //Will add event feature soon
         }
 
-        public void Train (TrainingData[] td, int iteration) {
-            for (int i = 0; i < iteration; i++) {
-                //This is a cpu intensive process, should be in a seperate thread
-                BackPropagation bp = new BackPropagation();
-                nlg = bp.BackPropagate(nlg, td);
-            }
+        public void Train (TrainingData[] td) {
+            BackPropagation bp = new BackPropagation();
+            nlg = bp.BackPropagate(nlg, td);
         }
 
         public float[] getError () {
@@ -71,17 +68,9 @@ namespace gSeer {
             return _rBuffer;
         }
 
-        private NeuronLayerGroup MutateWith (NeuronLayerGroup nlg) {
+        public NeuronLayerGroup MutateWith (NeuronLayerGroup nlg) {
             Genetics ga = new Genetics();
             return ga.Mutate(this.nlg, nlg);
-        }
-        /// <summary>
-        /// This will be used for while seer mutation
-        /// </summary>
-        /// <param name="seer"></param>
-        /// <returns></returns>
-        public Seer MutateWith (Seer seer) {
-            return seer;
         }
     }
 }
