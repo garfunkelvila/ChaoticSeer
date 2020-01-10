7b1881638bbfe540acec1665d363d1e3c570a044
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-10 13:37:57 +0800

    New class rng
    this is to prevent duplicate random numbers

---
+++ gSeer/rng.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gSeer {
+    static class rng {
+        static Random random = new Random();
+        internal static double getRng() {
+            return random.NextDouble();
+        }
+        internal static int getRngMinMax(int min, int max) {
+            return random.Next(min,max);
+        }
+    }
+}
