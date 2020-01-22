f21b9487f75bf15f4953c73b620b023ce8b68fe4
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-22 18:09:53 +0800

    Comment update

--- gSeer/Seer.cs
+++ gSeer/Seer.cs
@@ -25,7 +25,7 @@ using gSeer.Neuron;
 namespace gSeer {
     public class Seer : Genetics {
         public float Fitness { get; set; }
-        public NeuronLayerGroup NeuronLayerGroups { get; private set; }
+        public NeuronLayerGroup NeuronLayerGroups { get; private set; }     /// I made it like this in preperation for CNS
         readonly Back_Propagation.BackPropagation _BackPropagation;
 
         /// <summary>
@@ -37,28 +37,22 @@ namespace gSeer {
         /// <param name="mtBP">Use multi threading for back propagation?</param>
         public Seer (int inputCount, int outputCount, int numLayers = 1, bool mtBP = false) {
             _BackPropagation = SetThreadingMode(mtBP);
-            //if (mtBP) {
-            //    _BackPropagation = new BpMultiThread();
-            //}
-            //else {
-            //    _BackPropagation = new BpSingleThread();
-            //}
 
             NeuronLayer[] nL = new NeuronLayer[numLayers];
-            // If single layer
+            /// If single layer
             if (numLayers == 1) {
                 nL[0] = new NeuronLayer(inputCount, outputCount);
                 NeuronLayerGroups = new NeuronLayerGroup(nL);
                 return;
             }
-            // If multilayer
-            int hn = (int)Math.Ceiling(outputCount * 1.1d); // 1.1d to make the hidden layer a bit fat
-            nL[0] = new NeuronLayer(inputCount, hn);        // Add the first one
-            for (int nli = 1; nli < numLayers - 1; nli++) { // Skip the last
+            /// If multilayer
+            int hn = (int)Math.Ceiling(outputCount * 1.1d); /// 1.1d to make the hidden layer a bit fat
+            nL[0] = new NeuronLayer(inputCount, hn);        /// Add the first one
+            for (int nli = 1; nli < numLayers - 1; nli++) { /// Skip the last
                 nL[nli] = new NeuronLayer(hn, hn);
             }
-            nL[numLayers - 1] = new NeuronLayer(hn, outputCount);   // Add the last
-            NeuronLayerGroups = new NeuronLayerGroup(nL);     // add the layers to the group
+            nL[numLayers - 1] = new NeuronLayer(hn, outputCount);   /// Add the last
+            NeuronLayerGroups = new NeuronLayerGroup(nL);     /// add the layers to the group
         }
         /// <summary>
         /// This one should only be used on mutations
@@ -69,8 +63,8 @@ namespace gSeer {
         }
         public float[] Predict (float[] Sensories) {
             return NeuronLayerGroups.Predict(Sensories);
-            //This thing is supposed to raise an event when finished. But instead, I just return the value xD
-            //Will add event feature soon
+            /// This thing is supposed to raise an event when finished. But instead, I just return the value xD
+            /// Will add event feature soon
         }
 
         public void Train (TrainingData[] td, int iteration) {
@@ -95,6 +89,11 @@ namespace gSeer {
         public Seer MutateWith (Seer seer) {
             return new Seer(Mutate(this.NeuronLayerGroups, seer.NeuronLayerGroups));
         }
+        /// <summary>
+        /// Relocate this thing somewhere else
+        /// </summary>
+        /// <param name="isMultiThreaded"></param>
+        /// <returns></returns>
         private BackPropagation SetThreadingMode(bool isMultiThreaded = false) {
             if (isMultiThreaded)
                 return new BpMultiThread();
