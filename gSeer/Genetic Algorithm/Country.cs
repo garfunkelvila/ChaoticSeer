ff0e60e4cffb3b07560236f99109a02d91c9b982
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-24 15:48:05 +0800

    Attemp 1, FAILED

--- gSeer/Genetic Algorithm/Country.cs
+++ gSeer/Genetic Algorithm/Country.cs
@@ -29,15 +29,23 @@ namespace gSeer.Genetic_Algorithm {
                 Seers[i] = new Seer(_seer);
             }
         }
-        /// <summary>
-        /// Use this if there is a training data. Access the seer directly for
-        /// the chaotic simulation thing.
-        /// </summary>
-        public void Train(TrainingData[] trainingData) {
-            CalcFitness(trainingData );
-            Array.Sort(Seers);
-            Console.WriteLine("Sorting Test");
-        }
+		/// <summary>
+		/// Use this if there is a training data. Access the seer directly for
+		/// the chaotic simulation thing. For now it will be like Thanos 50:50
+		/// </summary>
+		/// <param name="trainingData"></param>
+		public void Train(TrainingData[] trainingData, int GenIteration) {
+			for (int i = 0; i < GenIteration; i++) {
+				/// This one fills the fitness for each seer for it to be sortable
+				CalcFitness(trainingData);
+				Array.Sort(Seers);
+				int Thanos = Seers.Length / 2; //Replace the half of population
+				for (int iSeer = Thanos; iSeer < Seers.Length; iSeer++) {
+					Seers[iSeer] = Seers[Util.GetRngMinMax(0, Thanos)].MutateWith(Seers[Util.GetRngMinMax(0, Thanos)]);
+				}
+			}
+			CalcFitness(trainingData);
+		}
 
         /// <summary>
         /// Get the predictions of all seer
@@ -57,32 +65,47 @@ namespace gSeer.Genetic_Algorithm {
         /// </summary>
         /// <param name="td"></param>
         public void CalcFitness(TrainingData[] td) {
-            /// less error difference, more fitness
-            /// Loop through all training data, no skip. More TD affects fitness
-            /// calculation with others
+			/// less error difference, more fitness
+			/// Loop through all training data, no skip. More TD affects fitness
+			/// calculation with others
 
-            /// Loop through seers in parallel
-            Parallel.For(0, Seers.Length, new ParallelOptions { MaxDegreeOfParallelism = Util.Cores }, i => {
-                float _fitnessBuffer = 0.0f;
-                /// Loop through training data
-                for (int iTd = 0; iTd < td.Length; iTd++) {
-                    float[] _predBuffer = Seers[i].Predict(td[iTd].Input);
-                    
-                    /// Loop through outputs and expected output
-                    for (int iPb = 0; iPb < _predBuffer.Length; iPb++) {
-                        /// Calc error and stack
-                        float _errBuffer;
-                        /// THis process may push positive numbers greately. Must find a better solution
-                        _errBuffer = td[iTd].Target[iPb] - _predBuffer[iPb];  /// Near zero is less error
-                        _errBuffer = Math.Abs(_errBuffer) * -1;             /// Make all negative
-                        _errBuffer = _errBuffer + 1;                        /// Offset
+			/// Loop through seers in parallel
+			Parallel.For(0, Seers.Length, new ParallelOptions { MaxDegreeOfParallelism = Util.Cores }, i => {
+				float _fitnessBuffer = 0.0f;
+				/// Loop through training data
+				for (int iTd = 0; iTd < td.Length; iTd++) {
+					float[] _predBuffer = Seers[i].Predict(td[iTd].Input);
 
-                        _fitnessBuffer += _errBuffer;
-                    }
-                }
-                Seers[i].Fitness = _fitnessBuffer;
-            });
+					/// Loop through outputs and expected output
+					for (int iPb = 0; iPb < _predBuffer.Length; iPb++) {
+						/// Calc error and stack
+						float _errBuffer;
+						/// THis process may push positive numbers greately. Must find a better solution
+						_errBuffer = td[iTd].Target[iPb] - _predBuffer[iPb];  /// Near zero is less error
+						_errBuffer = Math.Abs(_errBuffer) * -1;					/// Make all negative
+						//_errBuffer = _errBuffer + 1;                        /// Offset
 
-        }
+						_fitnessBuffer += _errBuffer;
+					}
+				}
+				Seers[i].Fitness = _fitnessBuffer;
+
+
+				//Also calc error
+				foreach (Seer item in Seers) {
+					int lastLayerIndex = item.NeuronLayerGroups.NeuronLayers.Length - 1;
+					//Average error on given td
+					for (int n = 0; n < item.NeuronLayerGroups.NeuronLayers[lastLayerIndex].Neurons.Length; n++) {
+						Neuron.Neuron[] neurons = item.NeuronLayerGroups.NeuronLayers[lastLayerIndex].Neurons;
+						float eBuffer = 0.0f;
+						for (int err = 0; err < td.Length; err++) {
+							eBuffer += td[err].Target[n] - neurons[n].Prediction;
+						}
+						neurons[n].Error = eBuffer;
+					}
+					
+				}
+			});
+		}
     }
 }
