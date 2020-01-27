287092d3a32da3c093325c5dfdd8a1b807fa84e6
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-27 23:12:32 +0800

    Prepare the mutation function
    Possibly the most comon mutation types i guess. Will remove neuron layers, and groups in future and use direct connections every neuron. Might add a way to track and compare, rank all of them

---
+++ gSeer/Genetic Algorithm/Genome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gSeer.Genetic_Algorithm {
+    public class Genome {
+        /// <summary>
+        /// Might be converted to seer in future
+        /// </summary>
+        public void MutateLink() {
+        }
+        public void MutateNode() {
+        }
+        public void MutateWeightShift() {
+        }
+        public void MutateWeightRandom() {
+        }
+        public void MutateToggleLink() {
+        }
+    }
+}
+
