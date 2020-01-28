64b930099047364ba9a13369d742878453657e72
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-28 23:08:20 +0800

    Create NodeGene.cs

---
+++ gSeer/Genetic Algorithm/NodeGene.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gSeer.Genetic_Algorithm {
+    public class NodeGene : Gene {
+}
