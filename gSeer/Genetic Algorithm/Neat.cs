de25434b33273a439f9f8bacee1ab10925b348a4
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-01-29 22:07:05 +0800

    Create neat class

---
+++ gSeer/Genetic Algorithm/Neat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gSeer.Genetic_Algorithm {
+    /// <summary>
+    /// Might rename this to CNS
+    /// </summary>
+    public class Neat {
+    }
+}
