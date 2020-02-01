62f1ec4ea1cbdb08b2e9b3e164c86233264dc9a7
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-02-01 23:59:49 +0800

    Update ConnectionGene.cs

--- gSeer/Genetic Algorithm/ConnectionGene.cs
+++ gSeer/Genetic Algorithm/ConnectionGene.cs
@@ -6,5 +6,13 @@ using System.Threading.Tasks;
 
 namespace gSeer.Genetic_Algorithm {
     public class ConnectionGene : Gene {
+        public NodeGene From { get; set; }
+        public NodeGene To { get; set; }
+        public float Weight { get; set; }
+        public bool IsEnabled { get; set; }
+        int InnovaitonNumber;   //I don't get how this thing is calculated
+        public ConnectionGene(bool isEnabled = true) {
+            IsEnabled = isEnabled;
+        }
     }
 }
