756217dbd92d262b035174de0f45f3778e0a5923
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-02-02 00:16:12 +0800

    Copy the common parameter settings

--- gSeer/Genetic Algorithm/Neat.cs
+++ gSeer/Genetic Algorithm/Neat.cs
@@ -9,5 +9,13 @@ namespace gSeer.Genetic_Algorithm {
     /// Might rename this to CNS
     /// </summary>
     public class Neat {
+        public const float WEIGHT_SHIFT_STRENGTH = 0.3f;
+        public const float WEIGHT_RANDOM_STRENGTH = 1.0f;
+
+        public const float PROBABILITY_MUTATE_CONNECTION = 0.005f;
+        public const float PROBABILITY_MUTATE_NODE = 0.003f;
+        public const float PROBABILITY_MUTATE_WEIGHT_SHIFT = 0.025f;
+        public const float PROBABILITY_MUTATE_WEIGHT_RANDOM = 0.0f;
+        public const float PROBABILITY_MUTATE_TOGGLE = 0.005f;
     }
 }
