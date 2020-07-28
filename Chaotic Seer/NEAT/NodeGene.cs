1990d3bc77d6887d2ed912d550e5b16dc03fdd94
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-28 22:03:40 +0800

    Create NodeGene.cs

---
+++ Chaotic Seer/NEAT/NodeGene.cs
@@ -0,0 +1,23 @@
+﻿using Chaotic_Seer.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.NEAT {
+	class NodeGene : INode{
+		public int Innovation { get; set; } // Even this thing is in list, it is used for hashing
+
+		public NodeGene(NodeGene neuron) {
+			Innovation = neuron.Innovation;
+		}
+		public override bool Equals(object obj) {
+			if (!GetType().Equals(obj.GetType())) return false;
+			NodeGene other = obj as NodeGene;
+			return Innovation == other.Innovation;
+		}
+
+		public override int GetHashCode() {
+			return Innovation;
+		}
+	}
+}
