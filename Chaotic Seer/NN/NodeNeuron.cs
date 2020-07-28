8deb53dfd1d30146e961508dcb47f1d2c83ef136
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-29 00:18:24 +0800

    Create concstructors for node neurons

--- Chaotic Seer/NN/NodeNeuron.cs
+++ Chaotic Seer/NN/NodeNeuron.cs
@@ -1,4 +1,5 @@
-﻿using Chaotic_Seer.Util;
+﻿using Chaotic_Seer.NEAT;
+using Chaotic_Seer.Util;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,13 @@ namespace Chaotic_Seer.NN {
 	class NodeNeuron : INode {
 		public int Innovation { get; set; }
 		public NeuronTypes Type { get; set; }
+
+		public NodeNeuron() {
+
+		}
+		public NodeNeuron(NodeGene node) {
+			Innovation = node.Innovation;
+			Type = node.Type;
+		}
 	}
 }
