99a199bbe963663ebd2ae5fc3663c2b78d3b5f12
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-29 01:09:51 +0800

    Add the main console
    NOthing here yet, just for testing things

---
+++ Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Main {
+	class Program {
+		static Test test = new Test();
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+			test.calc();
+		}
+
+	}
+
+	class Test {
+		int test = 10;
+
+		public void calc() {
+			int i = REFINT();
+			i = 100;
+        }
+
+
+		public ref int REFINT() {
+			return ref test;
+        }
+    }
+}
