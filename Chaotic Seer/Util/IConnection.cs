615d6bba368ddd9892744ed49567809bfc595271
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-28 21:50:38 +0800

    Attemp to implement a connection and node to be used on booth neat, and the calculator

---
+++ Chaotic Seer/Util/IConnection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.Util {
+    interface IConnection {
+        INode In { get; set; }
+        INode Out { get; set; }
+    }
+}
---
+++ Chaotic Seer/Util/INode.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.Util {
+    interface INode {
+    }
+}
