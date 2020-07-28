e3aa264f52cbfc325fda62d12497d82db78822b0
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-28 22:24:24 +0800

    Create connectors on booth neat and the calculator
    Calculator will always have the weights

---
+++ Chaotic Seer/NEAT/ConnectionGene.cs
@@ -0,0 +1,39 @@
+﻿using Chaotic_Seer.NN;
+using Chaotic_Seer.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.NEAT {
+	class ConnectionGene : IConnection {
+		public INode In { get; set; }
+		public INode Out { get; set; }
+		
+		ConnectionGene() {
+
+		}
+
+		public ConnectionGene(ConnectionGene item) {
+			In = item.In;
+			Out = item.Out;
+		}
+
+		public ConnectionGene(ConnectionNeuron item) {
+			In = item.In;
+			Out = item.Out;
+		}
+
+		public override bool Equals(object obj) {
+			if (!GetType().Equals(obj.GetType())) return false;
+
+			ConnectionGene other = obj as ConnectionGene;
+			return
+				In.Equals(other.In) &&
+				Out.Equals(other.Out);
+		}
+
+		public override int GetHashCode() {
+			return In.Innovation * Parameters.MaxNodes + Out.Innovation;
+		}
+	}
+}
---
+++ Chaotic Seer/NN/ConnectionNeuron.cs
@@ -0,0 +1,11 @@
+﻿using Chaotic_Seer.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.NN {
+    class ConnectionNeuron : IConnection {
+        public INode In { get; set; }
+        public INode Out { get; set; }
+    }
+}
