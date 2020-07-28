b3d7e260af6bdf9733ef7b0748700135842d2d4c
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-28 22:26:20 +0800

    Create the class for neuron or node on the calculator
    Empty for now

---
+++ Chaotic Seer/NN/NodeNeuron.cs
@@ -0,0 +1,11 @@
+﻿using Chaotic_Seer.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.NN {
+    class NodeNeuron : INode {
+        public int Innovation { get; set; }
+        public NeuronTypes Type { get; set; }
+    }
+}
