78481d511194513d534fae768c7bf6233c50d17a
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-28 21:20:31 +0800

    Create randomizer
    This is because I notices i get duplicate random numbers when using random inline. But this is way way slower

---
+++ Chaotic Seer/Util/Rng.cs
@@ -0,0 +1,50 @@
+﻿//  ChaoticSeer, a C# Artificial Neural Network Library
+//  Copyright (C) 2020  Garfunkel Vila
+//  
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 3 of the License, or any later version.
+//  
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  Lesser General Public License for more details.
+//  
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library. If not,
+//  see<https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.Util {
+	static internal class Rng {
+		public static readonly int Cores = Environment.ProcessorCount;
+		static readonly Random random = new Random();
+		//---------------------------------------------------------------------
+		internal static bool GetBool() {
+			return random.NextDouble() > 0.5d ? true : false;
+		}
+		internal static float GetFloat() {
+			return (float)random.NextDouble();
+		}
+		internal static double GetDouble() {
+			return random.NextDouble();
+		}
+		//---------------------------------------------------------------------
+		internal static int GetInt(int max) {
+			return random.Next(max);
+		}
+		internal static int GetIntMinMax(int min, int max) {
+			return random.Next(min, max);
+		}
+		internal static float GetRngMinMaxF(float min, float max) {
+			return (max - min) * (float)random.NextDouble() + min;
+		}
+		internal static float FloatingAnd(float x, float y) {
+			return x < y ? (x + 1) - y : (y + 1) - x;
+		}
+	}
+}
