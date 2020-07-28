0fd0b2459d16f78aaf256bc8a1add052fa7ddcab
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-29 02:01:17 +0800

    Copy the add population from master

--- Chaotic Seer/NEAT/Neat.cs
+++ Chaotic Seer/NEAT/Neat.cs
@@ -111,10 +111,28 @@ namespace Chaotic_Seer.NEAT {
 			}
 
 			foreach (Genome child in childrens) {
-				// AddToPopulation(child);
+				AddToPopulation(child);
 			}
 		}
 
-		
+		internal static void AddToPopulation(Genome genome) {
+			Genomes.Add(genome);
+
+			// Double check the connections
+			bool foundSpecies = false;
+			foreach (Specie specie in Species) {
+				if (!foundSpecies && genome.Equals(specie.Representative)) {
+					specie.AddGenome(genome);
+					foundSpecies = true;
+				}
+			}
+
+			if (!foundSpecies) {
+				Specie newSpecie = new Specie();
+				newSpecie.AddGenome(genome);
+				newSpecie.Representative = genome;
+				Species.Add(newSpecie);
+			}
+		}
 	}
 }
