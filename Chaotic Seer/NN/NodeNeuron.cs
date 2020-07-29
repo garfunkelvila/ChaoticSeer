c228a77bd90d8f158f75585761f15447374c1e1b
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-29 17:59:36 +0800

    Override Equals and HashCode

--- Chaotic Seer/NN/NodeNeuron.cs
+++ Chaotic Seer/NN/NodeNeuron.cs
@@ -8,13 +8,27 @@ namespace Chaotic_Seer.NN {
 	class NodeNeuron : INode {
 		public int Innovation { get; set; }
 		public NeuronTypes Type { get; set; }
+		public float Axon { get; set; }
 
 		public NodeNeuron() {
 
 		}
+
 		public NodeNeuron(NodeGene node) {
 			Innovation = node.Innovation;
 			Type = node.Type;
+		}
+
+		public override bool Equals(object obj) {
+			if (!GetType().Equals(obj.GetType())) return false;
+			NodeGene other = obj as NodeGene;
+			return
+				Innovation == other.Innovation &&
+				Type == other.Type;
+		}
+
+		public override int GetHashCode() {
+			return Innovation;
 		}
 	}
 }
