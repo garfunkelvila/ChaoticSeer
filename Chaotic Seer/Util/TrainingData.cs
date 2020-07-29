60abe5cbff641983ef5af1477048e19bf9ccf129
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-07-29 22:27:35 +0800

    Create TrainingData.cs
    This is to be used for genome input evaluation.
    Input: what is given to genome
    Output: error comparison

---
+++ Chaotic Seer/Util/TrainingData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chaotic_Seer.Util {
+    public class TrainingData {
+        float[] Input { get; }
+        float[] Output { get; }
+        public TrainingData(float[] Input, float[] Output) {
+            this.Input = Input;
+            this.Output = Output;
+        }
+    }
+}
