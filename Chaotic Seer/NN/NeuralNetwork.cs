627138a8461b62b20be5391641a397bd80fda762
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-08-01 16:39:35 +0800

    Prepare the calculator thing for the genome
    Basically the neural network

---
+++ Chaotic Seer/NN/NeuralNetwork.cs
@@ -0,0 +1,31 @@
+﻿using Chaotic_Seer.DataStructures;
+using Chaotic_Seer.NEAT;
+using Chaotic_Seer.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chaotic_Seer.NN {
+	/// <summary>
+	/// Use this for calculator
+	/// </summary>
+	static class NeuralNetwork {
+		//static internal DataHashSet<ConnectionNeuron> Connections = new DataHashSet<ConnectionNeuron>();
+		//static internal DataHashSet<NodeNeuron> Nodes = new DataHashSet<NodeNeuron>();
+		
+		/// <summary>
+		/// Calculate output and return output
+		/// </summary>
+		/// <param name="genome"></param>
+		public static void GetOutput(Genome genome) {
+		}
+
+		/// <summary>
+		/// Calculate output and evaluate fitness
+		/// </summary>
+		/// <param name="genome"></param>
+		public static void Evaluate(Genome genome, TrainingData td) {
+		}
+	}
+}
