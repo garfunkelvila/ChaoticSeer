10d6474946fb7420243f819aec50355c85d9ae42
Garfunkel Vila <garfunkelvila@users.noreply.github.com>
2020-08-05 18:27:34 +0800

    Update Program.cs

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,23 +1,34 @@
 ﻿using Chaotic_Seer.NEAT;
+using Chaotic_Seer.Util;
 using System;
+using System.Linq;
 
 namespace Main {
 	class Program {
 		static void Main(string[] args) {
-            Neat.Initialize();
+			Neat.Initialize();
 
+			Console.WriteLine("Population: " + Neat.Genomes.Count);
+			Console.WriteLine("Species: " + Neat.Species.Count);
 
-            Console.WriteLine("Population: " + Neat.Genomes.Count);
-            Console.WriteLine("Species: " + Neat.Species.Count);
-            for (int i = 0; i < 400; i++) {
-                Neat.Mutate();
-                Console.WriteLine("Gen: " + i);
-                Console.WriteLine("Population: " + Neat.Genomes.Count);
-                Console.WriteLine("Species: " + Neat.Species.Count);
-            }
+			TrainingData td = new TrainingData(
+					new float[2] { 1f, 0f},
+					new float[1] { 1f }
+				);
 
-            Console.ReadLine();
-        }
+			for (int i = 1; i < 400; i++) {
+				Genome[] genome = Neat.Genomes.ToArray();
+
+				Neat.Mutate();
+				Neat.Evaluate(td);
+				Neat.Purge();
+				Console.WriteLine("Gen: " + i);
+				Console.WriteLine("Population: " + Neat.Genomes.Count);
+				Console.WriteLine("Species: " + Neat.Species.Count);
+			}
+
+			Console.ReadLine();
+		}
 
 	}
 
